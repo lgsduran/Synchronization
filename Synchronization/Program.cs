569de6423bf913d4e5b7c6ca00dc8ef4bd2d6e96
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 <lgsduran@yahoo.com.br>
2024-03-09 19:26:49 -0300

    updated sleep

--- Synchronization/Program.cs
+++ Synchronization/Program.cs
@@ -38,7 +38,7 @@ class Program
         await scheduler.ScheduleJob(job, trigger);
 
         // some sleep to show what's happening
-        await Task.Delay(TimeSpan.FromSeconds(90));
+        await Task.Delay(TimeSpan.FromSeconds(120));
 
         // and last shut down the scheduler when you are ready to close your program
         await scheduler.Shutdown();
