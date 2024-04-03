2992f265e95b5b880823b9bcd9ec730e11529b0b
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 <lgsduran@yahoo.com.br>
2024-04-03 20:09:26 -0300

    added test

---
+++ SynchronizationTests/OperationSingleFileTest.cs
@@ -0,0 +1,64 @@
+﻿using Synchronization.Operations;
+using Synchronization.Settings;
+using Synchronization.Tests.Utils;
+using Synchronization.Utils;
+
+public class OperationSingleFileTest
+{
+    private readonly CheckSumUtils _checkSum = new();
+    private readonly WriteLogFileUtils _logFile = new();
+    private readonly OperationImpl _operation = new();
+    private readonly FileUtils _fileUtils = new();
+
+    private string? _source { get; set; } = String.Empty;
+    private string? _target { get; set; } = String.Empty;
+    private string checkSum { get; set; } = String.Empty;
+
+    [SetUp]
+    public void SetUp()
+    {
+        SettingOptions.GetSettings();
+        _source = FolderOptions.SourceFolder;
+        _target = FolderOptions.DestinationFolder;
+
+        if (string.IsNullOrWhiteSpace(_source) || string.IsNullOrWhiteSpace(_target))
+            throw new FilePathException("No empty path is allowed!");
+
+        _operation.InitializeOperation(_source, _target);
+    }
+
+    [Test]
+    public void CopySingleFileTest()
+    {
+        _fileUtils.FileWritter(_source + "test_1454.txt", false, "Copy");
+
+        _operation.Copy();
+
+        checkSum = _checkSum.SHA256CheckSum(FolderOptions.DestinationFolder + "test_1454.txt");
+
+        Assert.That(checkSum, Is.Not.Empty);
+    }
+
+    [Test]
+    public void CopyUpdatedSingleFileTest()
+    {
+        checkSum = _checkSum.SHA256CheckSum(FolderOptions.DestinationFolder + "test_1454.txt");
+
+        _fileUtils.FileWritter(_source + "test_1454.txt", true, "Update");
+
+        _operation.Update();
+
+        Assert.That(_checkSum.SHA256CheckSum(FolderOptions.DestinationFolder + "test_1454.txt"),
+            Is.Not.EqualTo(checkSum));
+    }
+
+    [Test]
+    public void DeleteSingleFileTest()
+    {
+        File.Delete(_source + "test_1454.txt");
+
+        _operation.Delete();
+
+        Assert.That(File.Exists(_target + "test_1454.txt"), Is.False);
+    }
+}
---
+++ SynchronizationTests/Utils/FileUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace Synchronization.Tests.Utils
+{
+    public class FileUtils
+	{
+        private StreamWriter sw = null!;
+
+        public void FileWritter(string filePath, bool append, string text)
+        {
+            Writter(filePath, append, text);
+        }
+
+        private void Writter(string filePath, bool append, string text)
+        {
+            try
+            {
+                sw = new StreamWriter(filePath, append);
+                sw.WriteLine(text);
+                sw.WriteLine("");
+            }
+            finally
+            {
+                if (sw is not null)
+                    sw.Dispose();
+            }
+        }
+    }
+}
+
