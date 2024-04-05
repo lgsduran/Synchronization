a3950adbe52584907570dd81d0b6ece45173f877
lgsduran <33518801+lgsduran@users.noreply.github.com>
2024-04-05 16:46:12 -0300

    Update folders.sh

--- SynchronizationTests/scripts/folders.sh
+++ SynchronizationTests/scripts/folders.sh
@@ -4,5 +4,6 @@ directories=("var/tmp/srcFolder" "var/tmp/destFolder")
 
 for directory in "${directories[@]}";
 do
-  mkdir -m 777 $directory
+  suso echo $directory
+  sudo mkdir -p $directory
 done
