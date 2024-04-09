3730853ccbce5e720e5858d1323ca06f0c0af541
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 <lgsduran@yahoo.com.br>
2024-04-09 15:48:49 -0300

    update solution paths

--- SynchronizationTests/scripts/folders.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-src_dir=/var/tmp/srcFolder
-dest_dir=/var/tmp/destFolder
-log_dir=/var/tmp/logs
-
-sudo mkdir -m 777 "${src_dir}"
-sudo mkdir -m 777 "${dest_dir}"
-sudo mkdir -m 777 "${log_dir}"
