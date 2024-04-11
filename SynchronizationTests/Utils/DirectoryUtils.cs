ec779a82cf6bd62ea0b3cebeefe43c56547b4f2c
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 <lgsduran@yahoo.com.br>
2024-04-10 23:44:49 -0300

    refactory

--- SynchronizationTests/Utils/DirectoryUtils.cs
+++ SynchronizationTests/Utils/DirectoryUtils.cs
@@ -13,29 +13,7 @@
             {
                 throw new Exception(e.Message);
             }
-        }
-
-        public void DirectoryI(string path)
-		{
-            DirectoryInfo di = new DirectoryInfo(path);
-            try
-            {
-               
-
-                // Try to create the directory.
-                di.Create();
-                Console.WriteLine("The directory was created successfully.");
-
-                // Delete the directory.
-                //di.Delete();
-                Console.WriteLine("The directory was deleted successfully.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The process failed: {0}", e.ToString());
-            }
-            finally { }
-        }
+        }      
 	}
 }
 
