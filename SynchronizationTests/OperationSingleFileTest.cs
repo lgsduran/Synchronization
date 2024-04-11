41b7f75e77cf69b3634e1854da0e2e8cca337b48
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 <lgsduran@yahoo.com.br>
2024-04-10 23:27:47 -0300

    Added tests

--- SynchronizationTests/OperationSingleFileTest.cs
+++ SynchronizationTests/OperationSingleFileTest.cs
@@ -11,28 +11,22 @@ public class OperationSingleFileTest
     private readonly WriteLogFileUtils _logFile = new();
     private readonly OperationImpl _operation = new();
     private readonly FileUtils _fileUtils = new();
-
-    private string? _source { get; set; } = String.Empty;
-    private string? _target { get; set; } = String.Empty;
     private string checkSum { get; set; } = String.Empty;
 
     [SetUp]
     public void SetUp()
     {
         SettingOptions.GetSettings();
-        _source = FolderOptions.SourceFolder;
-        _target = FolderOptions.DestinationFolder;
-
-        if (string.IsNullOrWhiteSpace(_source) || string.IsNullOrWhiteSpace(_target))
+        if (string.IsNullOrWhiteSpace(FolderOptions.SourceFolder) || string.IsNullOrWhiteSpace(FolderOptions.DestinationFolder))
             throw new FilePathException("No empty path is allowed!");
 
-        _operation.InitializeOperation(_source, _target);
+        _operation.InitializeOperation(FolderOptions.SourceFolder, FolderOptions.DestinationFolder);
     }
 
     [Test]
     public void CopySingleFileTest()
     {
-        _fileUtils.FileWritter(_source + "test_1454.txt", false, "Copy");
+        _fileUtils.FileWritter(FolderOptions.SourceFolder + "test_1454.txt", false, "Copy");
 
         _operation.Copy();
 
@@ -46,7 +40,7 @@ public class OperationSingleFileTest
     {
         checkSum = _checkSum.SHA256CheckSum(FolderOptions.DestinationFolder + "test_1454.txt");
 
-        _fileUtils.FileWritter(_source + "test_1454.txt", true, "Update");
+        _fileUtils.FileWritter(FolderOptions.SourceFolder + "test_1454.txt", true, "Update");
 
         _operation.Update();
 
@@ -57,10 +51,10 @@ public class OperationSingleFileTest
     [Test]
     public void DeleteSingleFileTest()
     {
-        File.Delete(_source + "test_1454.txt");
+        File.Delete(FolderOptions.SourceFolder + "test_1454.txt");
 
         _operation.Delete();
 
-        Assert.That(File.Exists(_target + "test_1454.txt"), Is.False);
+        Assert.That(File.Exists(FolderOptions.DestinationFolder + "test_1454.txt"), Is.False);
     }
 }
--- SynchronizationTests/OperationSingleFileUnderFolderTest.cs
+++ SynchronizationTests/OperationSingleFileUnderFolderTest.cs
@@ -13,7 +13,7 @@ public class OperationSingleFileUnderFolderTest
     private readonly FileUtils _fileUtils = new();
     private readonly DirectoryUtils _dirUtils = new();
 
-    private string? _folderPath { get; set; } = String.Empty;      
+    private string? _folderPath { get; set; } = String.Empty;
     private string checkSum { get; set; } = String.Empty;
 
     [SetUp]
@@ -25,13 +25,15 @@ public class OperationSingleFileUnderFolderTest
 
         _operation.InitializeOperation(FolderOptions.SourceFolder, FolderOptions.DestinationFolder);
 
-        _dirUtils.Folder(String.Format(@"{0}{1}", FolderOptions.SourceFolder, "source")).Create();
+        var dir = _dirUtils.Folder(String.Format(@"{0}{1}", FolderOptions.SourceFolder, "source"));
+
+        if (!dir.Exists) dir.Create();
 
         _folderPath = String.Format(@"{0}{1}", FolderOptions.SourceFolder, "source");
     }
 
     [Test]
-    public void CopySingleFileUnderFolderTest()
+    public void CopySingleFileToFolderTest()
     {
         _fileUtils.FileWritter(FolderOptions.SourceFolder + "test_1454.txt", false, "Copy");
         _fileUtils.FileWritter(String.Format(@"{0}/{1}", _folderPath, "test_1454.txt"), false, "Copy");
@@ -41,6 +43,55 @@ public class OperationSingleFileUnderFolderTest
         checkSum = _checkSum.SHA256CheckSum(String.Format(@"{0}{1}/{2}", FolderOptions.DestinationFolder, "source", "test_1454.txt"));
 
         Assert.That(checkSum, Is.Not.Empty);
-    }        
+    }
+
+    [Test]
+    public void CopyUpdatedFileToFolderTest()
+    {
+        checkSum = _checkSum.SHA256CheckSum(String.Format(@"{0}{1}/{2}", FolderOptions.DestinationFolder, "source", "test_1454.txt"));
+
+        _fileUtils.FileWritter(String.Format(@"{0}/{1}", _folderPath, "test_1454.txt"), false, "Updated folder");
+
+        _operation.Update();
+
+        Assert.That(_checkSum.SHA256CheckSum(String.Format(@"{0}{1}/{2}", FolderOptions.DestinationFolder, "source", "test_1454.txt")),
+            Is.Not.EqualTo(checkSum));
+    }
+
+    [Test]
+    public void validateRootFilesAreEqualsTest()
+    {
+        var srcFolder = _checkSum.SHA256CheckSum(FolderOptions.SourceFolder + "test_1454.txt");
+        var destFolder = _checkSum.SHA256CheckSum(FolderOptions.DestinationFolder + "test_1454.txt");      
+
+        Assert.That(srcFolder, Is.EqualTo(destFolder));
+    }
+
+    [Test]
+    public void DeleteFileFromFolderTest()
+    {
+        File.Delete(String.Format(@"{0}{1}/{2}", FolderOptions.SourceFolder, "source", "test_1454.txt"));
+
+        _operation.Delete();
+
+        Assert.That(File.Exists(String.Format(@"{0}{1}/{2}", FolderOptions.DestinationFolder, "source", "test_1454.txt")), Is.False);
+    }
+
+    [Test]
+    public void DeleteSourceFolderTest()
+    {
+        _dirUtils.Folder(String.Format(@"{0}{1}", FolderOptions.SourceFolder, "source")).Delete();
+
+        _operation.Delete();
+
+        Assert.That(_dirUtils.Folder(String.Format(@"{0}{1}", FolderOptions.DestinationFolder, "source")).Exists, Is.False);
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        File.Delete(FolderOptions.SourceFolder + "test_1454.txt");
+        _operation.Delete();
+    }
 }
 
